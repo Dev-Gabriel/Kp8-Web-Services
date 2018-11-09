7111fe36a405ffdb43b68518b07563047bc9aec4
Gabriel A. Montañez, Jr <Dev-Gabriel@users.noreply.github.com>
2018-11-09 10:12:26 +0800

    sheldon start

--- KP8Global_Client/api/controllers/sendout/dailyCTRMonitoring.js
+++ KP8Global_Client/api/controllers/sendout/dailyCTRMonitoring.js
@@ -10,7 +10,50 @@ idleSession = 'e_user_idle';
 
 var dailyCTR = function(req, mainCB)
 {
+    KPTN        = req.query.KPTN
+    TrxnType    = req.query.TrxnType
+    regioncode  = req.query.regioncode
+
     console.log('-------------- Daily CTR Monitoring -----------------')
+    console.log('KPTN       : ' +KPTN);
+    console.log('TrxnType   : ' +TrxnType);
+    console.log('regioncode : ' +regioncode);
+
+    var tableName = 
+
+    async.waterfall([
+        function(cb){
+            serverdate(cb);
+        },
+        function(serverdate){
+            console.log('serverdate');
+            console.log(serverdate);
+        }
+    ],function(error, response)
+    {
+        mainCB(null, response);
+    });
+}
+
+function serverdate(cb)
+{
+    async.waterfall([
+        function (cb){
+            getServerDate(kp7global, cb);
+        },function (getServerDate_response, cb){
+            if(getServerDate_response.code != 1){
+                response = getServerDate_response;
+                cb(true);
+            }
+            else {
+                serverDateTime = getServerDate_response.data.serverDateTime;
+                cb(false);
+            }
+            console.log(serverDateTime)
+        }
+    ],function(serverDateTime){
+        cb(serverDateTime)
+    })
 }
 
 module.exports = dailyCTR;
